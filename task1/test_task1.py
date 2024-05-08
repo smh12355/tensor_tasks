f52b7a53988adcc7706459bf6da7e759b484c453
smh <krik6162@gmail.com>
2024-05-08 12:12:55 +0500

    add .gitignore

--- task1/test_task1.py
+++ task1/test_task1.py
@@ -1,14 +1,6 @@
-import pytest
-from selenium import webdriver
 import Base
-
-@pytest.fixture
-def browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
 
 def test_human_strenght_search(browser):
     data = Base.TaskSolver(browser)
     data.driver.get(data.base_url)
-    assert data.FirstPhraseCheck(), "блок не содержится на странице"
+    assert data.FirstPhraseCheck()

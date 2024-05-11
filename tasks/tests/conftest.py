5b9b2ccffe5e0efc4bfa137e89baff4ff1b68a95
Smh <krik6162@gmail.com>
2024-05-11 17:00:17 +0500

    fix

--- tasks/tests/conftest.py
+++ tasks/tests/conftest.py
@@ -15,7 +15,10 @@ def setup_logging():
     debug_logger = logging.getLogger(__name__)
     debug_logger.setLevel(logging.DEBUG)
     current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    log_file_path = os.path.join(os.path.dirname(__file__), '..', 'logs',f"debug_log_{current_datetime}.log")
+    log_file_path = os.path.join(os.path.dirname(__file__),
+                                '..', 
+                                'logs',
+                                f"debug_log_{current_datetime}.log")
     debug_file_handler = logging.FileHandler(log_file_path)
     debug_file_handler.setLevel(logging.DEBUG)
     debug_file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
